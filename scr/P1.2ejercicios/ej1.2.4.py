1ce23072fe3dc6eaed82ae2e40578a76ec63fbbb
Jmunioz <jmunioz@iesrafaelalberti.es>
2023-11-22 19:48:20 +0100

    ok

---
+++ scr/P1.2ejercicios/ej1.2.4.py
@@ -0,0 +1,8 @@
+temperatura_grados=float(input("¿qué temperatura hace?"))
+print(temperatura_grados, "ºC") 
+
+grados_fahrenheit= (temperatura_grados+32)*5/9
+
+if print(temperatura_grados):
+    print("tu temperatura en fahrenheit es",grados_fahrenheit)
+
